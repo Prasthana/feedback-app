1450b2f8914b98c106f3f7b42671e954fc8cb54f
SirajAhmed22 <88759920+SirajAhmed22@users.noreply.github.com>
2024-05-24 12:29:23 +0530

    otp verify done

---
+++ feedbackapp/lib/constants.dart
@@ -0,0 +1,10 @@
+// TODO Implement this library.
+const String LOGIN = "Login";
+const String VERIFY_YOUR_EMAIL = "Verify Your Email";
+const String ENTER_CODE_SEND_TO = "Enter the code we sent to";
+const String CONFIRM = "Confirm";
+const String HAVE_NOT_RECEIVED_CODE = "Haven\'t received the code yet? ";
+const String RESEND = "Resend";
+
+
+
