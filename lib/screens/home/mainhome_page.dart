8b005d79c4c7aa38251a79cabb8611c21b90c89c
Nagaraju <nagaraju.kamatham@prasthana.com>
2024-05-27 12:27:05 +0530

    OneOnOneDateView - added

--- lib/screens/home/mainhome_page.dart
+++ lib/screens/home/mainhome_page.dart
@@ -1,5 +1,6 @@
 import 'package:feedbackapp/api_services/models/oneonones.dart';
 import 'package:feedbackapp/managers/apiservice_manager.dart';
+import 'package:feedbackapp/screens/oneOnOne/oneonone_from_date_view.dart';
 import 'package:feedbackapp/utils/helper_widgets.dart';
 import 'package:flutter/material.dart';
 
@@ -11,9 +12,9 @@ class MainHomePageView extends StatefulWidget {
 }
 
 class _MainHomePageViewState extends State<MainHomePageView> {
-
   // variable to call and store future list of posts
-  Future<List<OneOnOne>> oneOnOnesFuture = ApiManager.authenticated.fetchOneOnOnesList();
+  Future<List<OneOnOne>> oneOnOnesFuture =
+      ApiManager.authenticated.fetchOneOnOnesList();
 
 /*
   // function to fetch data from api and return future list of posts
@@ -30,49 +31,75 @@ class _MainHomePageViewState extends State<MainHomePageView> {
   }
   */
 
-
   @override
-Widget build(BuildContext context) {
-  return Scaffold(
-    body: Center(
-      child: FutureBuilder<List<OneOnOne>>(
-        future: oneOnOnesFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const CircularProgressIndicator();
-          } else if (snapshot.hasData) {
-            final oneOnOnesList = snapshot.data!;
-            return buildoneOnOnesList(oneOnOnesList);
-          } else {
-            return const Text("No data available");
-          }
-        },
-      ),
-    ),
-  );
-}
-  
-  Widget buildoneOnOnesList(List<OneOnOne> oneOnOnesList) {
-  return ListView.builder(
-    itemCount: oneOnOnesList.length,
-    itemBuilder: (context, index) {
-      final oneOnOne = oneOnOnesList[index];
-      return Container(
-        color: Colors.grey.shade300,
-        margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-        padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
-        height: 100,
-        width: double.maxFinite,
-        child: Row(
-          children: [
-            Expanded(flex: 1, child: Image.asset('assets/splash-image.png')),
-            addHorizontalSpace(10),
-            Expanded(flex: 3, child: Text(oneOnOne.participant?.name ?? "DUMMY")),
-          ],
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: FutureBuilder<List<OneOnOne>>(
+          future: oneOnOnesFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            } else if (snapshot.hasData) {
+              final oneOnOnesList = snapshot.data!;
+              return buildoneOnOnesList(oneOnOnesList);
+            } else {
+              return const Text("No data available");
+            }
+          },
         ),
-      );
-    },
-  );
-}
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          debugPrint('clickeed on calender ------>>>');
+          // Navigator.of(context).push(MaterialPageRoute(
+          //     builder: (context) => const OneOnOneDateView()));
+          // Navigator.of(context).push(MaterialPageRoute(builder: (context) =>  const MainTabView()));
 
+    Navigator.push(
+      context, // BuildContext of the current screen
+      MaterialPageRoute(builder: (context) => const OneOnOneDateView(), fullscreenDialog: true),
+    );
+
+  // showCupertinoModalBottomSheet(
+  //   context: context,
+  //   builder: (context) => OneOnOneDateView(),
+  // );  
+
+
+      //     showModalBottomSheet(
+      // context: context,
+      // builder: (context) => const OneOnOneDateView(),
+    //);
+
+        },
+        shape: const CircleBorder(),
+        child: const Icon(Icons.calendar_month_outlined),
+      ),
+    );
+  }
+
+  Widget buildoneOnOnesList(List<OneOnOne> oneOnOnesList) {
+    return ListView.builder(
+      itemCount: oneOnOnesList.length,
+      itemBuilder: (context, index) {
+        final oneOnOne = oneOnOnesList[index];
+        return Container(
+          color: Colors.grey.shade300,
+          margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+          padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+          height: 100,
+          width: double.maxFinite,
+          child: Row(
+            children: [
+              Expanded(flex: 1, child: Image.asset('assets/splash-image.png')),
+              addHorizontalSpace(10),
+              Expanded(
+                  flex: 3, child: Text(oneOnOne.participant?.name ?? "DUMMY")),
+            ],
+          ),
+        );
+      },
+    );
+  }
 }
---
+++ lib/screens/oneOnOne/oneonone_from_date_view.dart
@@ -0,0 +1,16 @@
+
+
+import 'package:flutter/material.dart';
+
+class OneOnOneDateView extends StatelessWidget {
+  const OneOnOneDateView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("1-on-1's"),
+      ),
+    );
+  }
+}
