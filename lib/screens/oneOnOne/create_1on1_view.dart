0a747f185d78fde9f61c52c2966ebd21b89db068
Nagaraju <nagaraju.kamatham@prasthana.com>
2024-05-28 15:49:45 +0530

    Update create_1on1_view.dart

--- lib/screens/oneOnOne/create_1on1_view.dart
+++ lib/screens/oneOnOne/create_1on1_view.dart
@@ -120,245 +120,248 @@ String _selectedOption = constants.doesNotRepeatText;
       backgroundColor: Colors.white,
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            addVerticalSpace(10.0),
-            meetingImage(),
-            const Text(
-              constants.selectEmployeeText,
-              style: TextStyle(
-                fontFamily: constants.uberMoveFont,
-                fontSize: 21,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-            addVerticalSpace(8.0),
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              height: 51.0,
-              child: TextButton(
-                onPressed: () {
-                  debugPrint("search employee ------>>>>");
-                  showCupertinoModalBottomSheet(
-                    context: context,
-                    builder: (context) => const SelectEmployeeView(),
-                  );
-                },
-                style: OutlinedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                  side:
-                      const BorderSide(color: constants.textColor, width: 1.0),
-                ),
-                child: const Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    constants.searchEmployeeText,
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                      color: constants.textColor,
-                      fontFamily: constants.uberMoveFont,
-                      fontSize: 14,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              addVerticalSpace(10.0),
+              meetingImage(),
+              const Text(
+                constants.selectEmployeeText,
+                style: TextStyle(
+                  fontFamily: constants.uberMoveFont,
+                  fontSize: 21,
+                  fontWeight: FontWeight.w500,
                 ),
               ),
-            ),
-            addVerticalSpace(26),
-
-            /// *************Date start***************
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      constants.dateText,
+              addVerticalSpace(8.0),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: 51.0,
+                child: TextButton(
+                  onPressed: () {
+                    debugPrint("search employee ------>>>>");
+                    showCupertinoModalBottomSheet(
+                      context: context,
+                      builder: (context) => const SelectEmployeeView(),
+                    );
+                  },
+                  style: OutlinedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    side:
+                        const BorderSide(color: constants.textColor, width: 1.0),
+                  ),
+                  child: const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      constants.searchEmployeeText,
+                      textAlign: TextAlign.left,
                       style: TextStyle(
+                        color: constants.textColor,
                         fontFamily: constants.uberMoveFont,
-                        fontSize: 21,
-                        fontWeight: FontWeight.w500,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
                       ),
                     ),
-                    addVerticalSpace(8),
-                    SizedBox(
-                      width: (MediaQuery.of(context).size.width / 2) - 28,
-                      height: 51.0,
-                      child: TextButton(
-                        onPressed: () {
-                          debugPrint("select date ------>>>>");
-                          _selectDate(context);
-                        },
-                        style: OutlinedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8.0),
-                          ),
-                          side: const BorderSide(
-                              color: constants.textColor, width: 1.0),
-                        ),
-                        child: Align(
-                          alignment: Alignment.centerLeft,
-                          child: Text(
-                            DateFormat('dd-MM-yyyy').format(selectedDate),
-                            textAlign: TextAlign.left,
-                            style: const TextStyle(
-                              color: constants.textColor,
-                              fontFamily: constants.uberMoveFont,
-                              fontSize: 14,
-                              fontWeight: FontWeight.w400,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
-                // *************Date end***************
-
-                addHorizontalSpace(24),
-
-                // *************Time start***************
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      constants.timeText,
-                      style: TextStyle(
-                        fontFamily: constants.uberMoveFont,
-                        fontSize: 21,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                    addVerticalSpace(8),
-                    SizedBox(
-                      width: (MediaQuery.of(context).size.width / 2) - 28,
-                      height: 51.0,
-                      child: TextButton(
-                        onPressed: () {
-                          debugPrint("select time ------>>>>");
-                          _selectTime();
-                        },
-                        style: OutlinedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8.0),
-                          ),
-                          side: const BorderSide(
-                              color: constants.textColor, width: 1.0),
-                        ),
-                        child: Align(
-                          alignment: Alignment.centerLeft,
-                          child: Text(
-                            "${selectedTime.hour} : ${selectedTime.minute}",
-                            textAlign: TextAlign.left,
-                            style: const TextStyle(
-                              color: constants.textColor,
-                              fontFamily: constants.uberMoveFont,
-                              fontSize: 14,
-                              fontWeight: FontWeight.w400,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            // *************Time end***************
-            addVerticalSpace(18),
-            GestureDetector(
-              onTap: () {
-                debugPrint('GestureDetector ------->>>>');
-                _showRadioButtonDialog();
-              },
-              child: Row(
+              ),
+              addVerticalSpace(26),
+          
+              /// *************Date start***************
+              Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  const Text(
-                    constants.repeatText,
-                    style: TextStyle(
-                      fontFamily: constants.uberMoveFont,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w500,
-                    ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        constants.dateText,
+                        style: TextStyle(
+                          fontFamily: constants.uberMoveFont,
+                          fontSize: 21,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      addVerticalSpace(8),
+                      SizedBox(
+                        width: (MediaQuery.of(context).size.width / 2) - 28,
+                        height: 51.0,
+                        child: TextButton(
+                          onPressed: () {
+                            debugPrint("select date ------>>>>");
+                            _selectDate(context);
+                          },
+                          style: OutlinedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8.0),
+                            ),
+                            side: const BorderSide(
+                                color: constants.textColor, width: 1.0),
+                          ),
+                          child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              DateFormat('dd-MM-yyyy').format(selectedDate),
+                              textAlign: TextAlign.left,
+                              style: const TextStyle(
+                                color: constants.textColor,
+                                fontFamily: constants.uberMoveFont,
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                  addHorizontalSpace(10),
-                  Text(
-                    _selectedOption,
-                    style: const TextStyle(
-                      fontFamily: constants.uberMoveFont,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w400,
-                    ),
+                  // *************Date end***************
+          
+                  addHorizontalSpace(24),
+          
+                  // *************Time start***************
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        constants.timeText,
+                        style: TextStyle(
+                          fontFamily: constants.uberMoveFont,
+                          fontSize: 21,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      addVerticalSpace(8),
+                      SizedBox(
+                        width: (MediaQuery.of(context).size.width / 2) - 28,
+                        height: 51.0,
+                        child: TextButton(
+                          onPressed: () {
+                            debugPrint("select time ------>>>>");
+                            _selectTime();
+                          },
+                          style: OutlinedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8.0),
+                            ),
+                            side: const BorderSide(
+                                color: constants.textColor, width: 1.0),
+                          ),
+                          child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              "${selectedTime.hour} : ${selectedTime.minute}",
+                              textAlign: TextAlign.left,
+                              style: const TextStyle(
+                                color: constants.textColor,
+                                fontFamily: constants.uberMoveFont,
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                  addHorizontalSpace(5),
-                  const Icon(
-                    Icons.arrow_drop_down,
-                    color: Colors.black,
-                  )
                 ],
               ),
-            ),
-
-            addVerticalSpace(30),
-            const Text(
-              constants.notesText,
-              style: TextStyle(
-                fontFamily: constants.uberMoveFont,
-                fontSize: 21,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-
-            addVerticalSpace(8),
-
-            TextFormField(
-              minLines: 5,
-              maxLines: 5,
-              keyboardType: TextInputType.multiline,
-              decoration: const InputDecoration(
-                hintText: constants.notesHintText,
-                hintStyle: TextStyle(color: Colors.grey, fontSize: 14),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(8.0)),
+              // *************Time end***************
+              addVerticalSpace(18),
+              GestureDetector(
+                onTap: () {
+                  debugPrint('GestureDetector ------->>>>');
+                  _showRadioButtonDialog();
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const Text(
+                      constants.repeatText,
+                      style: TextStyle(
+                        fontFamily: constants.uberMoveFont,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    addHorizontalSpace(10),
+                    Text(
+                      _selectedOption,
+                      style: const TextStyle(
+                        fontFamily: constants.uberMoveFont,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    addHorizontalSpace(5),
+                    const Icon(
+                      Icons.arrow_drop_down,
+                      color: Colors.black,
+                    )
+                  ],
                 ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: constants.textColor,
+              ),
+          
+              addVerticalSpace(30),
+              const Text(
+                constants.notesText,
+                style: TextStyle(
+                  fontFamily: constants.uberMoveFont,
+                  fontSize: 21,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+          
+              addVerticalSpace(8),
+          
+              TextFormField(
+                minLines: 5,
+                maxLines: 5,
+                keyboardType: TextInputType.multiline,
+                textInputAction: TextInputAction.done,
+                decoration: const InputDecoration(
+                  hintText: constants.notesHintText,
+                  hintStyle: TextStyle(color: Colors.grey, fontSize: 14),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: constants.textColor,
+                    ),
                   ),
                 ),
+                style: const TextStyle(
+                  fontFamily: constants.uberMoveFont,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                ),
               ),
-              style: const TextStyle(
-                fontFamily: constants.uberMoveFont,
-                fontSize: 14,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-
-            addVerticalSpace(30),
-
-            MaterialButton(
-              minWidth: double.infinity,
-              height: 58.0,
-              onPressed: () {
-                debugPrint("clicked on create ----->>>>");
-                
-                
-              },
-              // ignore: sort_child_properties_last
-              child: const Text(constants.createText),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(5.0),
-              ),
-              color:const Color.fromRGBO(0, 0, 0, 1),
-              //const Color.fromRGBO(173, 173, 173, 1),
-              textColor: Colors.white,
-            )
-          ],
+          
+              addVerticalSpace(30),
+          
+              MaterialButton(
+                minWidth: double.infinity,
+                height: 58.0,
+                onPressed: () {
+                  debugPrint("clicked on create ----->>>>");
+                  
+                  
+                },
+                // ignore: sort_child_properties_last
+                child: const Text(constants.createText),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+                color:const Color.fromRGBO(0, 0, 0, 1),
+                //const Color.fromRGBO(173, 173, 173, 1),
+                textColor: Colors.white,
+              )
+            ],
+          ),
         ),
       ),
     );
