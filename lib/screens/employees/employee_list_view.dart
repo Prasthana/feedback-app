429a99f431f1b460a3a8835080b008f645965605
SirajAhmed22 <88759920+SirajAhmed22@users.noreply.github.com>
2024-05-28 14:35:28 +0530

    Update employee_list_view.dart

--- lib/screens/employees/employee_list_view.dart
+++ lib/screens/employees/employee_list_view.dart
@@ -1,4 +1,3 @@
-
 import 'package:feedbackapp/api_services/models/employee.dart';
 import 'package:feedbackapp/api_services/models/employeesresponse.dart';
 import 'package:feedbackapp/managers/apiservice_manager.dart';
@@ -15,137 +14,143 @@ class EmployeeListView extends StatefulWidget {
 }
 
 class _EmployeeListViewState extends State<EmployeeListView> {
-  Future<EmployeesResponse> employeesFuture = ApiManager.authenticated.fetchEmployeesList();
-  
+  Future<EmployeesResponse> employeesFuture =
+      ApiManager.authenticated.fetchEmployeesList();
+
   @override
   Widget build(BuildContext context) {
     Theme.of(context);
 
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar( title: const Text(constants.reportingTeamTitle)),
+      appBar: AppBar(title: const Text(constants.reportingTeamTitle)),
       body: Center(
-      child: FutureBuilder<EmployeesResponse>(
-        future: employeesFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const CircularProgressIndicator();
-          } else if (snapshot.hasData) {
-            final employeesResponse = snapshot.data;
+        child: FutureBuilder<EmployeesResponse>(
+          future: employeesFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            } else if (snapshot.hasData) {
+              final employeesResponse = snapshot.data;
               var listCount = employeesResponse?.employeesList?.length ?? 0;
               if (listCount > 0) {
                 return buildEmployeesList(employeesResponse?.employeesList);
               } else {
                 return buildEmptyListView();
               }
-          } else {
+            } else {
               return buildEmptyListView();
-          }
-        },
+            }
+          },
+        ),
       ),
-    ),
     );
   }
-  
 
-String getInitials(String string, [int limitTo = 2]) {
-  if (string == null || string.isEmpty) {
-    return '';
+  String getInitials(String string, [int limitTo = 2]) {
+    if (string == null || string.isEmpty) {
+      return '';
+    }
+
+    var buffer = StringBuffer();
+    var split = string.split(' ');
+
+    //For one word
+    if (split.length == 1) {
+      return string.substring(0, 1);
+    }
+
+    for (var i = 0; i < (limitTo ?? split.length); i++) {
+      buffer.write(split[i][0]);
+    }
+
+    return buffer.toString();
   }
-
-  var buffer = StringBuffer();
-  var split = string.split(' ');
-
-  //For one word
-  if (split.length == 1) {
-    return string.substring(0, 1);
-  }
-
-  for (var i = 0; i < (limitTo ?? split.length); i++) {
-    buffer.write(split[i][0]);
-  }
-
-  return buffer.toString();
-}
 
   Widget buildEmployeesList(List<Employee>? employeeList) {
     return ListView.builder(
-          itemCount: employeeList?.length,
-          itemBuilder: (BuildContext context, int index) {
-            final employee = employeeList?[index];
+        itemCount: employeeList?.length,
+        itemBuilder: (BuildContext context, int index) {
+          final employee = employeeList?[index];
 
-            return Column(
-        children: <Widget>[
-          ListTile(
+          return Column(
+            children: <Widget>[
+              ListTile(
                 // leading:  TextDrawable(text: employee?.name ?? ""),
                 leading: CircleAvatar(
-                backgroundColor: themeconstants.colorPrimary,
-                maxRadius: 28.0,
-                foregroundImage: NetworkImage(""),
-                child: Text(getInitials(employee?.name ?? "", 2),
-                style: const TextStyle(
-                    fontFamily: constants.uberMoveFont,
-                    fontSize: 17,
-                    fontWeight: FontWeight.w500,
-                    color: Color.fromRGBO(255, 255, 255, 1)),
-                    ),
+                  backgroundColor: themeconstants.colorPrimary,
+                  maxRadius: 28.0,
+                  foregroundImage: NetworkImage("https://picsum.photos/250?image=9"),
+                  child: Text(
+                    getInitials(employee?.name ?? "", 2),
+                    style: const TextStyle(
+                        fontFamily: constants.uberMoveFont,
+                        fontSize: 17,
+                        fontWeight: FontWeight.w500,
+                        color: Color.fromRGBO(255, 255, 255, 1)),
+                  ),
                 ),
                 trailing: const Icon(Icons.chevron_right),
-                title: Text(employee?.name ?? "",
-                style: const TextStyle(
-                    fontFamily: constants.uberMoveFont,
-                    fontSize: 17,
-                    fontWeight: FontWeight.w700,
-                    color: Color.fromRGBO(0, 0, 0, 1)),),
-                subtitle: Text(employee?.designation ?? "",
-                 style: const TextStyle(
-                    fontFamily: constants.uberMoveFont,
-                    fontSize: 13,
-                    fontWeight: FontWeight.w500,
-                    color: Color.fromRGBO(0, 0, 0, 1)),),
+                title: Text(
+                  employee?.name ?? "",
+                  style: const TextStyle(
+                      fontFamily: constants.uberMoveFont,
+                      fontSize: 17,
+                      fontWeight: FontWeight.w700,
+                      color: Color.fromRGBO(0, 0, 0, 1)),
                 ),
-          const Divider(
-            color: Color.fromRGBO(195, 195, 195, 1),
+                subtitle: Text(
+                  employee?.designation ?? "",
+                  style: const TextStyle(
+                      fontFamily: constants.uberMoveFont,
+                      fontSize: 13,
+                      fontWeight: FontWeight.w500,
+                      color: Color.fromRGBO(0, 0, 0, 1)),
+                ),
+              ),
+              const Divider(
+                color: Color.fromRGBO(195, 195, 195, 1),
                 height: 3.0,
                 thickness: 1.0,
-                indent: 76.0, 
+                indent: 76.0,
                 endIndent: 0,
-          ),
-        ],
-      );
-      });
+              ),
+            ],
+          );
+        });
   }
 
-
   Widget buildEmptyListView() {
-    return
-              Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-child:
-     Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-               Image.asset('assets/emptyOneOnOneList.png', height: 250),
-          addVerticalSpace(20),
-          const Text('No Employees Added',style: TextStyle (
-                    fontFamily: constants.uberMoveFont,
-                    fontSize: 20,
-                    fontStyle: FontStyle.normal,
-                    fontWeight: FontWeight.w500,
-                    ),
+    return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset('assets/emptyOneOnOneList.png', height: 250),
+              addVerticalSpace(20),
+              const Text(
+                'No Employees Added',
+                style: TextStyle(
+                  fontFamily: constants.uberMoveFont,
+                  fontSize: 20,
+                  fontStyle: FontStyle.normal,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              addVerticalSpace(20),
+              const Text(
+                'You can add employees  by clicking on the plus icon below.',
+                style: TextStyle(
+                  fontFamily: constants.uberMoveFont,
+                  fontSize: 15,
+                  fontStyle: FontStyle.normal,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ],
           ),
-          addVerticalSpace(20),
-          const Text(
-                  'You can add employees  by clicking on the plus icon below.',style: TextStyle (
-                    fontFamily: constants.uberMoveFont,
-                    fontSize: 15,
-                    fontStyle: FontStyle.normal,
-                    fontWeight: FontWeight.w400,
-                    ),),
-        ],
-      ),
-    ));
+        ));
   }
 }
