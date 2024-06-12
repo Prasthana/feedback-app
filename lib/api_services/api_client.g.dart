926521e83e87a2156362bb8e00e0976e9ce0ef9c
sriLatha2328 <106054180+sriLatha2328@users.noreply.github.com>
2024-06-12 14:00:46 +0530

    Feedback app clientId,clientSecret and base Url changes

--- lib/api_services/api_client.g.dart
+++ lib/api_services/api_client.g.dart
@@ -13,7 +13,7 @@ class _ApiClient implements ApiClient {
     this._dio, {
     this.baseUrl,
   }) {
-    baseUrl ??= 'http://ec2-18-219-231-99.us-east-2.compute.amazonaws.com/';
+    baseUrl ??= ApiManager.baseURL;
   }
 
   final Dio _dio;
--- lib/managers/apiservice_manager.dart
+++ lib/managers/apiservice_manager.dart
@@ -6,7 +6,7 @@ import 'package:oneononetalks/api_services/dio-addOns/dio_client.dart';
 
 class ApiManager {
   static const String baseURL =
-      'http://ec2-18-219-231-99.us-east-2.compute.amazonaws.com/';
+      'http://feedback-staging-alb-818086335.us-east-2.elb.amazonaws.com/';
 
   static final authenticated = ApiClient(buildDioClient(baseURL));
   static final public = ApiClient(buildDioClient(baseURL));
