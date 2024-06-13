a14977c43af3dcad1ae7deab0357ffd45909a88a
sriLatha2328 <106054180+sriLatha2328@users.noreply.github.com>
2024-06-13 15:51:08 +0530

    client_id,client_secerct and base url changes

--- lib/managers/apiservice_manager.dart
+++ lib/managers/apiservice_manager.dart
@@ -5,8 +5,14 @@ import 'package:oneononetalks/api_services/dio-addOns/dio_client.dart';
 
 
 class ApiManager {
+
+  // staging credentials
+  // static const String baseURL =
+  //     'http://feedback-staging-alb-818086335.us-east-2.elb.amazonaws.com/';
+
+  // Production credentials
   static const String baseURL =
-      'http://feedback-staging-alb-818086335.us-east-2.elb.amazonaws.com/';
+      'http://feedback-production-alb-314267296.us-east-2.elb.amazonaws.com/';
 
   static final authenticated = ApiClient(buildDioClient(baseURL));
   static final public = ApiClient(buildDioClient(baseURL));
