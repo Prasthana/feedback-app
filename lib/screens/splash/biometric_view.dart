523648d52e2ddd23d1f17133a7c6e8016476d92e
Nagaraju <nagaraju.kamatham@prasthana.com>
2024-06-20 14:04:25 +0530

    Update biometric_view.dart

--- lib/screens/splash/biometric_view.dart
+++ lib/screens/splash/biometric_view.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:local_auth/local_auth.dart';
@@ -33,21 +34,55 @@ class _BiometricViewState extends State<BiometricView> {
         ),
       );
     } on PlatformException catch (e) {
+      if (e.message == constants.biometricNotEnable) {
+        // ignore: use_build_context_synchronously
+        showValidationAlert(context, constants.biometricNotEnableAlertText);
+      }
       print("authenticate state ----->>>>${e.message}");
       return;
     }
     if (!mounted) {
+       print("mounted ----->>>>");
       return;
     }
 
       setState(() {
         if (authenticated) {
-             // print("authenticate sucess ----->>>>");
+              print("authenticate sucess ----->>>>");
               Navigator.of(context).popUntil((route) => route.isFirst);
         } else {
-            //  print("authenticate failed ----->>>>");
+              print("authenticate failed ----->>>>");
         }
       });
+  }
+
+    showValidationAlert(BuildContext context, String alertText) {
+    // set up the button
+    Widget okButton = TextButton(
+      child: const Text(constants.okButton),
+      onPressed: () {
+        Navigator.pop(context);
+        Navigator.pop(context);
+      },
+    );
+
+    Widget cancelButton = TextButton(
+      child: const Text(constants.goToSettings),
+      onPressed: () {
+        Navigator.pop(context);
+      },
+    );
+
+    CupertinoAlertDialog alert = CupertinoAlertDialog(
+      content: Text(alertText),
+      actions: [okButton, cancelButton],
+    );
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
   }
 
   @override
